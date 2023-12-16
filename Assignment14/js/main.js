d9725aa215fe7db8cd7c2090f0f086131bbe2866
Taylor Draper <143915603+taykdra@users.noreply.github.com>
2023-12-15 17:12:15 -0700

    Update main.js

--- Assignment14/js/main.js
+++ Assignment14/js/main.js
@@ -1,12 +1,10 @@
 var c = document.getElementById("book?");
 var ctx = c.getContext("2d");
 // Drawing a rectangle
-ctx.beginPath();
+ctx.beginPath;
 ctx.moveTo(300, 50);
 ctx.lineTo(500, 50);
 ctx.lineTo(500, 150);
 ctx.lineTo(300, 150);
-ctx.closePath();
-ctx.fillStyle = "blue";
-ctx.fill();
-ctx.stroke();
+ctx.closePath;
+ctx.fillStyle = "black";
